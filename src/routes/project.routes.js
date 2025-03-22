460fb55c3b124f2d6639cd2d0bcb0a3ce13358c9
Esteban Ortiz <Esteban.ortiz.dev@gmail.com>
2025-03-21 21:13:31 -0500

    Agrega rutas para gestión de proyectos con autenticación y control de roles

---
+++ src/routes/project.routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const projectController = require('../controllers/project.controller');
+const ROLES = require('../utils/constants');
+const {authenticateToken, checkRole}= require('../middlewares/auth.middleware');
+
+router.post('/projects/create', authenticateToken, checkRole(ROLES.ADMIN), projectController.createProject);
+router.put('/projects/update/:id', authenticateToken, checkRole(ROLES.ADMIN), projectController.updateProject);
+router.get('/projects', authenticateToken, checkRole(ROLES.ADMIN), projectController.getAllProjects);
+router.delete('/projects/delete/:id', authenticateToken, checkRole(ROLES.ADMIN), projectController.deleteProject);
+router.get('/projects/rol/:id', authenticateToken, checkRole(ROLES.USER), projectController.getAllProjectsByUserId);
+
+router.post('/projects/assign/:projectId', authenticateToken, checkRole(ROLES.ADMIN), projectController.assignUserToProject);
+router.delete('/projects/unassign/:projectId', authenticateToken, checkRole(ROLES.ADMIN), projectController.removeUserFromProject);
+
+module.exports = router;
